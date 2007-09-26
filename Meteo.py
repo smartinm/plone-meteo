3006fefc3518bf1362d01fd1cf2b26ae382fcd7f
Manuel Jesús Recena Soto <recena@klicap.es>
2007-09-26 10:24:18 +0000

    Preparando el producto para publicarlo en plone.org

--- Meteo.py
+++ Meteo.py
@@ -13,16 +13,33 @@ funcionar ante cualquier cambio en el mismo. Sin embargo, para su tranquilidad
 la página no ha cambiado durante años.
 
 Dependencias:
- BeautifulSoup [http://www.crummy.com/software/BeautifulSoup]
+	BeautifulSoup [http://www.crummy.com/software/BeautifulSoup]
 
-Autor:
- Sergio Martín <smartin@gmv.com>
+Meteo
+Copyright (C) 2007 GMV SGI Team <http://www.gmv-sgi.es>
+ 
+This program is free software; you can redistribute it and/or
+modify it under the terms of version 2 of the GNU General Public
+License as published by the Free Software Foundation.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+USA
+  
+$Id$
+  
 """
 
 __author__ = "Sergio Martín (smartin@gmv.com)"
 __version__ = "0.1.0"
-__copyright__ = "Copyright (c) 2007 Sergio Martín"
-__license__ = "GPL"
+__copyright__ = "Copyright (C) 2007 GMV SGI Team <http://www.gmv-sgi.es>"
+__license__ = "GPLv2"
 
 import re
 import socket
